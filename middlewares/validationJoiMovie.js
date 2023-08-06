ce2c31554efdf1c605a14c23592e54d736695403
Shinkarenko Anton <AVEINDOMINUSNOX@yandex.ru>
2023-08-06 15:21:14 +0500

    fix after first review

--- middlewares/validationJoiMovie.js
+++ middlewares/validationJoiMovie.js
@@ -1,5 +1,6 @@
 const { celebrate, Joi } = require('celebrate');
-const { urlRegex } = require('../utils/constants');
+
+const urlRegex = /[(http(s)?)://(www.)?a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&//=]*)/;
 
 const validationCreateMovie = celebrate({
   body: Joi.object().keys({
